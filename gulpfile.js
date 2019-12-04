710747b0a126f35eb876d6b760995cd886302ed7
Dmitry <petrov6827@gmail.com>
2019-12-05 02:18:08 +0300

    clients1

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const svgSprite = require('gulp-svg-sprite');
-
-gulp.task('svgSprite', function () {
-    return gulp.src('/Users/polzovatel/Documents/projects/sprite/*.svg') // svg files for sprite
-        .pipe(svgSprite({
-            mode: {
-                stack: {
-                    sprite: "../sprite.svg"  //sprite file name
-                }
-            },
-        }
-        ))
-        .pipe(gulp.dest('/Users/polzovatel/Documents/projects/Photograph/img/'));
-});
 
 gulp.task('sass', function () {
     return gulp.src('./sass/**/*.scss')
